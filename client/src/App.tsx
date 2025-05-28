ecd568e7dc0122f6556f0bf487c62cd59e4c181e
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2025-05-28 22:43:08 +0200

    fix: more domain changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,8 +40,8 @@ function App() {
     <div className="h-screen w-screen flex flex-col gap-10 p-5">
       <header className="text-xl">
         Portfolio [
-        <a href="https://rokaskasperavicius.dev" target="_blank">
-          rokaskasperavicius.dev
+        <a href="https://rokas.site" target="_blank">
+          rokas.site
         </a>
         ]
       </header>
