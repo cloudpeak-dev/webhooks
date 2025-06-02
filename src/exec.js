e1011c44dea5f2f3b9319bb02ebe43881a7a45ae
Rokas Kasperavicius <github.rokaskasperavicius@gmail.com>
2025-06-02 23:57:28 +0200

    break on purpose

--- src/exec.js
+++ src/exec.js
@@ -21,6 +21,8 @@ export const exec = (type, command, successCallback) => {
       // Strip ANSI codes
       const cleanData = stripAnsi(data.toString());
       log.append(cleanData);
+
+      console.log(log.getDate());
     });
 
     spawn_process.stderr.on("data", (data) => {
