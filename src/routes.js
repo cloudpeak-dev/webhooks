0de905d061e391bb2ea1de8e24e905b8d54e6796
Rokas Kasperavicius <github.rokaskasperavicius@gmail.com>
2025-06-10 22:10:17 +0200

    feat: added mongo logging for cache invalidation events

--- src/routes.js
+++ src/routes.js
@@ -102,14 +102,31 @@ app.post("/datocms/rebuild", async (req, res) => {
 app.post("/datocms/invalidate-cache", async (req, res) => {
   const token = req.query.token;
 
+  const startDate = new Date();
+  let success = true;
+
   try {
     await axios.post("https://www.rokas.site/api/invalidate-cache", null, {
       params: { token },
     });
   } catch (error) {
+    success = false;
     logger.error(error);
-    res.status(500).send("Something went wrong");
+  } finally {
+    const endDate = new Date();
+    const runningTimeInSeconds = Math.abs(endDate - startDate) / 1000;
 
+    await insertLog({
+      type: "datocms-invalidate-cache",
+      start_date: startDate,
+      end_date: startDate,
+      running_time_in_seconds: runningTimeInSeconds,
+      success,
+    });
+  }
+
+  if (!success) {
+    res.status(500).send("Failed to invalidate cache");
     return;
   }
 
